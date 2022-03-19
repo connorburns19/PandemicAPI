70108ea5aee16c564356bf913b67e64718295f56
peterbilski <peter.bilski@mail.utoronto.ca>
2022-03-18 21:51:00 -0400

    got rid of server directory for heroku

---
+++ server.js
@@ -0,0 +1,13 @@
+// Boilerplate from V V V:
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/development_environment
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Test')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
